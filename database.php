a22b3e0476f9aeb9e44b591bbb202f103024134f
su apt install kuma <maxxozou@gmail.com>
2025-03-01 12:19:36 +0100

    Enhance database connection handling with TLS support and improved error messages

--- database.php
+++ database.php
@@ -1,40 +1,45 @@
 <?php
 
-    require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
+use Dotenv\Dotenv;
 
-    use Dotenv\Dotenv;
+class ConnexionDB {
+    private $pdo;
 
-    class ConnexionDB {
-        private $pdo;
+    public function __construct() {
+        $dotenv = Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
 
-        public function __construct() {
-            $dotenv = Dotenv::createImmutable(paths: __DIR__);
-            $dotenv->load();
+        $dbHost = $_ENV['DB_HOST'];
+        $dbName = $_ENV['DB_NAME'];
+        $dbUser = $_ENV['DB_USER'];
+        $dbPassword = $_ENV['DB_PASSWORD'];
 
-            $dbHost = $_ENV['DB_HOST'];
-            $dbName = $_ENV['DB_NAME'];
-            $dbUser = $_ENV['DB_USER'];
-            $dbPassword = $_ENV['DB_PASSWORD'];
+        try {
+            //Connexion TLS
+            $dsn = "mysql:host=$dbHost;dbname=$dbName;charset=utf8mb4";
+            
+            $options = [
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ];
 
-            try {
-                $dsn = "mysql:host=$dbHost;dbname=$dbName;charset=utf8mb4";
-                $this->pdo = new PDO($dsn, $dbUser, $dbPassword);
+            $this->pdo = new PDO($dsn, $dbUser, $dbPassword, $options);
 
-                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-            } catch (PDOException $e) {
-                echo "Erreur de connexion : " . $e->getMessage();
-                exit;
-            }
-        }
-
-        public function DB() {
-            return $this->pdo;
-        }
-
-        public function closeConnection() {
-            $this->pdo = null;
+        } catch (PDOException $e) {
+            echo "Une erreur est survenue. Veuillez réessayer plus tard.";
+            exit;
         }
     }
+
+    public function DB() {
+        return $this->pdo;
+    }
+
+    public function closeConnection() {
+        $this->pdo = null;
+    }
+}
 ?>
